59b853f9cba9fec5b3f1fb943c1afc24d6a48cb1
Adhish1612Acharya <adhisha0505@gmail.com>
2024-10-25 21:40:12 +0530

    Commented email sending js code

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,27 +1,17 @@
-const sendEmail = () => {
-  document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    console.log("submitted");
-    event.preventDefault();
-    // these IDs from the previous steps
-    emailjs.sendForm("service_0af8oll", "template_6tbc1pa", this).then(
-      () => {
-        console.log("SUCCESS!");
-      },
-      (error) => {
-        console.log("FAILED...", error);
-      }
-    );
-  });
-
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", (event) => {
-      event.preventDefault();
-      console.log("submitted  ");
-    });
-};
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     // these IDs from the previous steps
+//     emailjs.sendForm("service_0af8oll", "template_6tbc1pa", this).then(
+//       () => {
+//         console.log("SUCCESS!");
+//       },
+//       (error) => {
+//         console.log("FAILED...", error);
+//       }
+//     );
+//   });
 
 const bootstrapValidation = (() => {
   "use strict";
@@ -34,12 +24,21 @@ const bootstrapValidation = (() => {
     form.addEventListener(
       "submit",
       (event) => {
+        event.preventDefault();
         if (!form.checkValidity()) {
-          console.log(forms);
-          event.preventDefault();
           event.stopPropagation();
         } else {
-          sendEmail();
+          emailjs.init({
+            publicKey: "a0Dannguw0kzPA30D",
+          });
+          emailjs.sendForm("service_0af8oll", "template_6tbc1pa", this).then(
+            () => {
+              console.log("SUCCESS!");
+            },
+            (error) => {
+              console.log("FAILED...", error);
+            }
+          );
         }
         // } else {
         //   document
