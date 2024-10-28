edabf3fa484f1cb8d8dc1e4f8ba0014a7e1296b1
Adhish1612Acharya <adhisha0505@gmail.com>
2024-10-28 14:39:56 +0530

    Added the js file to check the user log in status for home page of admin file

--- js/admin/home.js
+++ js/admin/home.js
@@ -1,6 +1,7 @@
 import { auth } from "../firebase.js";
 
-const pageLoader = document.querySelector("#pageLoader");
+const pageLoader = document.querySelector(".pageLoader");
+console.log(pageLoader);
 
 pageLoader.style.display = "block";
 
@@ -13,5 +14,23 @@ const isLogin = () => {
     }
   });
 };
+
+const logout = async () => {
+  await auth
+    .signOut()
+    .then(() => {
+      console.log("User  signed out");
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      console.error("Error signing out:", error);
+    });
+};
+
+const logOut = document.querySelector("#logout");
+
+logOut.addEventListener("click", () => {
+  logout();
+});
 
 window.onload = isLogin();
