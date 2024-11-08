a5a068817ffd358cc0c7a941eab08eaa802bb982
Vijeth-Kumar-18 <vijethkumar3110@gmail.com>
2024-11-08 16:30:09 +0530

    Started to update the Event and Add Event page

--- js/firebase.js
+++ js/firebase.js
@@ -11,22 +11,23 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCUaQIV4_ZQsnReSZT4qfjn6btUDAw-GoI",
-//   authDomain: "challengers-66645.firebaseapp.com",
-//   projectId: "challengers-66645",
-//   storageBucket: "challengers-66645.appspot.com",
-//   messagingSenderId: "202449545843",
-//   appId: "1:202449545843:web:b02596feba5560c89b1806",
-//   measurementId: "G-P315CS7PTG",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCUaQIV4_ZQsnReSZT4qfjn6btUDAw-GoI",
+  authDomain: "challengers-66645.firebaseapp.com",
+  projectId: "challengers-66645",
+  storageBucket: "challengers-66645.appspot.com",
+  messagingSenderId: "202449545843",
+  appId: "1:202449545843:web:b02596feba5560c89b1806",
+  measurementId: "G-P315CS7PTG",
+};
 
 // firebaseConfig
 
+
 // Initialize Firebase
 console.log(window.FIREBASE_CONFIG);
-// const app = initializeApp(firebaseConfig);
-const app = initializeApp(window.FIREBASE_CONFIG);
+const app = initializeApp(firebaseConfig);
+// const app = initializeApp(window.FIREBASE_CONFIG);
 // const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
@@ -34,3 +35,7 @@ export const db = getFirestore(app);
 export const auth = getAuth();
 
 export default app;
+
+
+
+
