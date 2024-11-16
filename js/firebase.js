09e9440974af9f98b1146b2b304770d97f4ae3b6
Vijeth-Kumar-18 <vijethkumar3110@gmail.com>
2024-11-16 17:22:18 +0530

    added some css

--- js/firebase.js
+++ js/firebase.js
@@ -14,17 +14,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 // firebaseConfig
 
 //firebasehide 
-const firebaseConfig = {
-  apiKey: "AIzaSyCUaQIV4_ZQsnReSZT4qfjn6btUDAw-GoI",
-  authDomain: "challengers-66645.firebaseapp.com",
-  projectId: "challengers-66645",
-  storageBucket: "challengers-66645.firebasestorage.app",
-  messagingSenderId: "202449545843",
-  appId: "1:202449545843:web:b02596feba5560c89b1806",
-  measurementId: "G-P315CS7PTG",
- };
-
-const app = initializeApp( firebaseConfig);
+const app = initializeApp(window.FIREBASE_CONFIG);
 
 
 export const db = getFirestore(app);
