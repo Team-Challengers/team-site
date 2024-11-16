b8c8e6b1ce73c40affeee5bff7678fbb0792a800
Vijeth-Kumar-18 <vijethkumar3110@gmail.com>
2024-11-16 17:06:37 +0530

    made changes in Event register

--- js/firebase.js
+++ js/firebase.js
@@ -14,7 +14,17 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 // firebaseConfig
 
 //firebasehide 
-const app = initializeApp(window.FIREBASE_CONFIG);
+const firebaseConfig = {
+  apiKey: "AIzaSyCUaQIV4_ZQsnReSZT4qfjn6btUDAw-GoI",
+  authDomain: "challengers-66645.firebaseapp.com",
+  projectId: "challengers-66645",
+  storageBucket: "challengers-66645.firebasestorage.app",
+  messagingSenderId: "202449545843",
+  appId: "1:202449545843:web:b02596feba5560c89b1806",
+  measurementId: "G-P315CS7PTG",
+ };
+
+const app = initializeApp( firebaseConfig);
 
 
 export const db = getFirestore(app);
