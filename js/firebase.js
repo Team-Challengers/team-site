f3be86d39ce015784bd2e7788570d9e3a8a23a6c
Vijeth-Kumar-18 <vijethkumar3110@gmail.com>
2024-11-16 15:53:13 +0530

    Added Admin Event Registration Viewing PaGe

--- js/firebase.js
+++ js/firebase.js
@@ -13,8 +13,18 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUaQIV4_ZQsnReSZT4qfjn6btUDAw-GoI",
+  authDomain: "challengers-66645.firebaseapp.com",
+  projectId: "challengers-66645",
+  storageBucket: "challengers-66645.firebasestorage.app",
+  messagingSenderId: "202449545843",
+  appId: "1:202449545843:web:b02596feba5560c89b1806",
+  measurementId: "G-P315CS7PTG",
+ };
 // firebaseConfig
-const app = initializeApp(window.FIREBASE_CONFIG);
+
+const app = initializeApp( firebaseConfig);
 
 
 export const db = getFirestore(app);
